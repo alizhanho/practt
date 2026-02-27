86e30d6efabb5b28573f78977accb4322862536b
AlizhanAmanzhol <alizanamanzol249@gmail.com>
2026-02-28 00:00:54 +0500

    Exercises

--- prac3/lambda/lambda_with_filter.py
+++
@@ -1,6 +0,0 @@
-numbers = [10, 15, 20, 23, 30]
-
-# Filter even numbers
-evens = list(filter(lambda x: x % 2 == 0, numbers))
-
-print(evens)
--- prac3/lambda/lambda_with_map.py
+++
@@ -1,6 +0,0 @@
-numbers = [1, 2, 3, 4, 8]
-
-# Square each number
-squared = list(map(lambda x: x ** 2, numbers))
-
-print(*squared)
